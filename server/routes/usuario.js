6b3abe0dd02b4d93c527453ebc9114646388503a
dgonzalezq2 <52825682+dgonzalezq2@users.noreply.github.com>
2020-07-01 13:01:31 -0500

    integración de paquete underscore y modificación de usuarios.js en carpeta routes

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,21 +1,51 @@
-const express = require("express");
-const bcrypt = require('bcrypt')
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore')
 const Usuario = require('../models/usuario');
-
+//const usuario = require('../models/usuario');
 const app = express();
 
-app.get("/usuario", (req, res) => {
-    res.json("get Usuario");
+app.get('/usuario', function(req, res) {
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 0;
+    limite = Number(limite);
+
+    Usuario.find({}, `nombre email role estado img google`)
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.count({}, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    numero: conteo
+                });
+            });
+
+
+        });
+
 });
 
-app.post("/usuario", (req, res) => {
+app.post('/usuario', function(req, res) {
+
     let body = req.body;
 
-    let usuario = new Usuario({ //Creando un objeto Usuario
+    let usuario = new Usuario({
         nombre: body.nombre,
         email: body.email,
         password: bcrypt.hashSync(body.password, 10),
         role: body.role
+
     });
 
     usuario.save((err, usuarioDB) => {
@@ -23,25 +53,46 @@ app.post("/usuario", (req, res) => {
             return res.status(400).json({
                 ok: false,
                 err
-            })
+            });
         }
 
-        usuarioDB.password = null;
-
+        // usuarioDB.password=null;
         res.json({
             ok: true,
             usuario: usuarioDB
         });
     });
+
+});
+//cambiar datos de recurso existentes en el servidor 
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email,', 'img', 'role', 'estado'])
+
+    //busqueda del ususario que necesito encontrar y actualizar
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+
+
 });
 
-app.put("/usuario/:id", (req, res) => {
-    let id = req.param.id;
-    res.json(id);
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario');
 });
 
-app.delete("/usuario", (req, res) => {
-    res.json("delete Usuario");
-});
+module.exports = app;
 
 module.exports = app;
