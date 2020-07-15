a963a23092de23d38813397433d061e2586fea93
dgonzalezq2 <52825682+dgonzalezq2@users.noreply.github.com>
2020-07-15 13:17:07 -0500

    JWB agregado

--- server/server.js
+++ server/server.js
@@ -1,30 +1,27 @@
-require('./config/config');
-const express = require("express");
+require('./config/config')
+
+const express = require('express');
 const mongoose = require('mongoose');
+
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-//Incluir rutas de usuario
+// incluir configuración global de las rutas
+app.use(require('./routes/index'));
 
-app.use(require('./routes/usuario'))
-
-
-mongoose.connect(process.env.URLDB, {
-            useCreateIndex: true,
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        },
-        (err, res) => {
-            if (err) throw err;
-            console.log("Base de datos ONLINE");
-        }) //Conectar con MongoDB
+// Conectar con MongoDB
+mongoose.connect(process.env.URLDB, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true },
+    (err, res) => {
+        if (err) throw err;
+        console.log("Base de Datos ONLINE!");
+    });
 
 app.listen(process.env.PORT, () => {
-    console.log("Escuchando en el puerto: ", process.env.PORT);
+    console.log("Escuchando en el puerto", process.env.PORT);
 });
